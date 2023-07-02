123a57341956b6fc7f7c10c9830e6189607d5a77
Seyi Majekobaje <blackgeeknerd@gmail.com>
2023-07-02 12:21:57 +0100

    update

--- bincomPro/settings.py
+++ bincomPro/settings.py
@@ -77,30 +77,18 @@ WSGI_APPLICATION = 'bincomPro.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # } ,
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'bincompoll$default',
-    #     'USER': 'bincompoll',
-    #     'PASSWORD': 'striker147',
-    #     'HOST': 'bincompoll.mysql.pythonanywhere-services.com',
-    #     'PORT': '3306',
-    #     'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
-    # }
-
-    #for development
+   
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bincomphptest',
-        'USER': 'root',
-        'PASSWORD': 'striker',
-        'HOST': 'localhost',
+        'NAME': 'bincompoll$default',
+        'USER': 'bincompoll',
+        'PASSWORD': 'striker147',
+        'HOST': 'bincompoll.mysql.pythonanywhere-services.com',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
+
+   
 }
 
 
