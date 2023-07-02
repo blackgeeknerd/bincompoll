3f2b7467acf201a18c7440d187429929e6ea0eb5
Seyi Majekobaje <blackgeeknerd@gmail.com>
2023-07-02 21:23:47 +0100

    lgaresults

--- bincomApp/forms.py
+++ bincomApp/forms.py
@@ -8,3 +8,44 @@ class NewPollingUnit(forms.ModelForm):
     fields = ["polling_unit_id", "ward_id", "lga_id", "uniquewardid", "polling_unit_number", "polling_unit_name", "polling_unit_description", "lat", "long", "entered_by_user", "user_ip_address"]
     labels = {'polling_unit_id': "Unit Id", 'ward_id' : "Ward Id", 'lga_id' : "LGA Id", 'uniquewardid' : "UnqWard Id", 'polling_unit_number' : "Unit Number", 'polling_unit_name' : "Unit Name", 'polling_unit_description' : "Unit Description", 'lat' : "LAT", 'long' : "LONG", 'entered_by_user' : "Staff Name"}
     
+
+class LocalGovtNames(forms.Form):
+  CHOICES = (
+     ('Aniocha North', 'Aniocha North'),
+    ('Aniocha - South', 'Aniocha - South'),
+    ('Ethiope East', 'Ethiope East'),
+    ('Ethiope West', 'Ethiope West'),
+    ('Ika North - East', 'Ika North - East'),
+    ('Ika - South', 'Ika - South'),
+    ('Isoko North', 'Isoko North'),
+    ('Ndokwa East','Ndokwa East'),
+    ('Ndokwa West', 'Ndokwa West'),
+    ('Okpe', 'Okpe'),
+    ('Oshimili - North', 'Oshimili - North'),
+    ('Isoko South', 'Isoko South'),
+    ('Oshimili - South', 'Oshimili - South'),
+    ('Patani', 'Patani'),
+    ('Sapele', 'Sapele'),
+    ('Udu', 'Udu'),
+    ('Ughelli North', 'Ughelli North'),
+    ('Ughelli South',  'Ughelli South'),
+    ('Ukwuani', 'Ukwuani'),
+    ('Uvwie', 'Uvwie'),
+    ('Bomadi', 'Bomadi'),
+    ('Burutu', 'Burutu'),
+    ('Warri North', 'Warri North'),
+    ('Warri South', 'Warri South'),
+    ('Warri South West', 'Warri South West'),
+  )
+  
+  local_govt_name = forms.ChoiceField(choices=CHOICES)
+
+
+
+
+
+
+
+
+
+
--- bincomApp/models.py
+++ bincomApp/models.py
@@ -27,19 +27,6 @@ class AnnouncedLgaResults(models.Model):
         db_table = 'announced_lga_results'
 
 
-class AnnouncedPuResults(models.Model):
-    result_id = models.AutoField(primary_key=True)
-    polling_unit_uniqueid = models.CharField(max_length=50)
-    party_abbreviation = models.CharField(max_length=4)
-    party_score = models.IntegerField()
-    entered_by_user = models.CharField(max_length=50)
-    date_entered = models.DateTimeField()
-    user_ip_address = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'announced_pu_results'
-
-
 class AnnouncedStateResults(models.Model):
     result_id = models.AutoField(primary_key=True)
     state_name = models.CharField(max_length=50)
@@ -130,4 +117,20 @@ class Ward(models.Model):
         db_table = 'ward'
 
 
+class AnnouncedPuResults(models.Model):
+    belongs_to = models.ForeignKey(PollingUnit, on_delete=models.CASCADE, null=True, blank=True)
+    result_id = models.AutoField(primary_key=True)
+    polling_unit_uniqueid = models.CharField(max_length=50)
+    party_abbreviation = models.CharField(max_length=4)
+    party_score = models.IntegerField()
+    entered_by_user = models.CharField(max_length=50)
+    date_entered = models.DateTimeField()
+    user_ip_address = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'announced_pu_results'
+
+
+class Test(models.Model):
+    pass
 
